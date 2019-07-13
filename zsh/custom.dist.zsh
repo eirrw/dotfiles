48a3dae86f8b3a8363aaa7b6cb385c672fa2e0f2
Erik Wilson <erikwilson@outlook.com>
2019-07-13 15:51:16 -0400

    Fix tmux autostart

--- zsh/custom.dist.zsh
+++ zsh/custom.dist.zsh
@@ -1,10 +1,9 @@
-# source default zshrc
-source ~/.dotfiles/zshrc
-
 # Autostart Tmux on shell launch
 # Uncomment to activate. Recommended only for workstations
 #export ZSH_TMUX_AUTOSTART=true
 
+# source default zshrc
+source ~/.dotfiles/zshrc
 # add custom commands below here
 ################################
 
