3b0f1a30a7675dbf97251c56245adc6c1c109bf6
virunus <tech+git@virunus.com>
2021-02-20 13:51:33 -0500

    remove now unneeded syntax file

--- zsh/syntax.zsh
+++
@@ -1,5 +0,0 @@
-# syntax highlighting (must be at end of file)
-# Include this file at the end of the main zshrc
-source ~/.dotfiles/zsh/history-search-multi-word/history-search-multi-word.plugin.zsh
-source ~/.dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-
