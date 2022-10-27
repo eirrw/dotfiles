4d884f2205a9865b9d7ce2f2264c5e5a00996a3c
Eir Wilson <tech+git@virunus.com>
2022-10-27 15:12:49 -0400

    add some additional lsp configs

--- roles/neovim/files/lua/config/nvim-lspconfig.lua
+++ roles/neovim/files/lua/config/nvim-lspconfig.lua
@@ -3,20 +3,33 @@ local lsp = require('lspconfig')
 
 vim.diagnostic.config({ virtual_text = true })
 
-local servers = {
-    pylsp = {},
-    gopls = {},
-    tailwindcss = {},
-    html = {},
-}
-
+local on_attach = function(client, bufnr)
+    local bufopts = { noremap=true, silent=true, buffer=bufnr}
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
 
 lsp.pylsp.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
 }
 
 lsp.tailwindcss.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach
 }
 
 lsp.gopls.setup {
@@ -29,9 +42,37 @@ lsp.gopls.setup {
             staticcheck = true,
         },
     },
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
 }
 
 lsp.html.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+
+lsp.texlab.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+
+lsp.sumneko_lua.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                globals = {'vim'},
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
