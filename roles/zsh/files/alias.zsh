21ed2eb7f20c993e9c1986352bcfadd76aaa2c60
Eir Wilson <tech+git@virunus.com>
2022-12-09 13:09:21 -0500

    zsh: update ls/exa aliasing

--- roles/zsh/files/alias.zsh
+++ roles/zsh/files/alias.zsh
@@ -3,15 +3,21 @@ alias d="dirs -v"
 for index ({1..9}) alias "$index"="cd +${index}"; unset index # directory stack
 
 # ls
-alias ls="ls --color=auto"
-alias la="ls -la"
-alias ll="ls -l"
+exa_opts=""
+ls_cmd=ls
+if [ -x "$(command -v exa)" ]; then
+    ls_cmd=exa
+    exa_opts="--git --icons"
+fi
+
+alias ls="${ls_cmd} --color=auto ${exa_opts}"
+alias la="${ls_cmd} -la ${exa_opts}"
+alias ll="${ls_cmd} -l ${exa_opts}"
 
 # ssh
 alias s="ssh"
 
 # command replacements
-[[ -x "$(command -v exa)" ]] && alias ls=exa
 [[ -x "$(command -v rg)" ]] && alias grep=rg
 [[ -x "$(command -v rsync)" ]] && \
     alias cp='rsync -ahPHAXSb --backup-dir=/tmp/rsync -e /dev/null'
