8c86e50ac6bff12e12bbabb03e0a68d880580e4e
Eir Wilson <tech+git@virunus.com>
2023-01-07 13:03:53 -0500

    neovim: add lsp and syntax for ts/js

--- roles/neovim/files/lua/config/nvim-lspconfig.lua
+++ roles/neovim/files/lua/config/nvim-lspconfig.lua
@@ -82,3 +82,13 @@ lsp.rust_analyzer.setup {
     capabilities = capabilities,
     on_attach = on_attach,
 }
+
+lsp.eslint.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+
+lsp.tsserver.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
