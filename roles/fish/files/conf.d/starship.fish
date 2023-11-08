edb2199a695c3ba8f6e3304e48c12b2f40245b45
Eir Wilson <tech+git@virunus.com>
2023-11-08 10:08:33 -0500

    starship: add config for non-unicode tty; enable_transience

--- roles/fish/files/conf.d/starship.fish
+++ roles/fish/files/conf.d/starship.fish
@@ -1,4 +1,19 @@
 if status is-interactive
-    command -q starship
-    and starship init fish | source
+    and command -q starship
+
+    if string match -q -r '^/dev/tty[0-9]+$' (tty)
+        # plaintext
+        set -x STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship/tty.toml"
+    else
+        # unicode / nerdfonts
+        set -x STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship/pts.toml"
+    end
+
+    # transient prompt leader
+    function starship_transient_prompt_func
+        starship module character
+    end
+
+    starship init fish | source
+    enable_transience
 end
