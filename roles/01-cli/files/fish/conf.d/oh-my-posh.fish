0a22b448e9ecc1f42d207d6fd3d2cd601f5b4afc
Eir Wilson <git@eirrw.com>
2024-12-03 15:38:41 -0500

    remove omp config, add ssh-agent setup

--- roles/01-cli/files/fish/conf.d/oh-my-posh.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    #    set dir (default $XDG_CONFIG_HOME ~/.config)
-    #
-    #    command -q oh-my-posh
-    #    and oh-my-posh init fish --config "$dir/oh-my-posh.toml" | source
-end
--- roles/01-cli/files/fish/functions/rm.fish
+++ roles/01-cli/files/fish/functions/rm.fish
@@ -1,4 +1,4 @@
 function rm
-    echo "Do not use rm! Use tp instead: tp $argv"
-    echo "Use /usr/bin/rm is absolutely necessary"
+    echo "Do not use rm! Use trash-put instead: trash-put $argv"
+    echo "Use /usr/bin/rm if absolutely necessary"
 end
