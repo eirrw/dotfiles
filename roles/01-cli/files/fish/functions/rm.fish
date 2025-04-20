addf0952a2203bf2d9382eabca7d3d97caf75cb6
Eir Wilson <git@eirrw.com>
2025-04-19 21:25:18 -0400

    fish and ansible infrastructure updates
    fish: switch to fisher for 'plugin' management
    ansible: use variable task to allow copying files to remote hosts

--- roles/01-cli/files/fish/functions/rm.fish
+++ roles/01-cli/files/fish/functions/rm.fish
@@ -1,4 +1,10 @@
-function rm
+function rm --wraps=rm
+    if set -q ALLOW_RM; and test "$ALLOW_RM" -eq 1
+        echo "permanently deleting files..."
+        command rm $argv
+        return
+    end
+
     echo "Do not use rm! Use trash-put instead: trash-put $argv"
-    echo "Use /usr/bin/rm if absolutely necessary"
+    echo "Use 'command rm' if absolutely necessary, or set ALLOW_RM=1"
 end
