2a73b4159b6e3aa94f8503e6fae160f699feb2db
Eir Wilson <git@eirrw.com>
2026-04-24 11:33:46 -0400

    update homebrew env and change DE message

--- roles/01-cli/files/fish/conf.d/brew.fish
+++ roles/01-cli/files/fish/conf.d/brew.fish
@@ -8,4 +8,7 @@ if test -e "/home/linuxbrew/.linuxbrew/bin/brew"
     if test -d (brew --prefix)"/share/fish/vendor_completions.d"
         set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
     end
+
+    set -x HOMEBREW_NO_ENV_HINTS 1
+    set -x HOMEBREW_AUTO_UPDATE_SECS 25200
 end
