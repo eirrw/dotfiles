125519ac94849911257f6bc115fb1e57014460b8
Eir Wilson <git@eirrw.com>
2026-09-01 14:37:19 -0400

    Fix issues passing paths to 'ls'

--- roles/01-cli/files/fish/functions/ls.fish
+++ roles/01-cli/files/fish/functions/ls.fish
@@ -1,6 +1,6 @@
 function ls
     if command -q eza
-        eza --color=auto --git --icons $argv
+        eza --color=auto --git --icons=auto $argv
     else
         command ls --color=auto $argv
     end
